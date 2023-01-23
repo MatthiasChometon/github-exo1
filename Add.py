28b6cf790272c427deb0cd6d54e00c3d498d526c
MatthiasChometon <matthiaschometon787@gmail.com>
2023-01-23 10:13:09 +0100

    feat: add Bottle

---
+++ Add.py
@@ -0,0 +1,12 @@
+def add(a, b):
+    """
+    Given two integers, return the sum.
+
+    :param a: int
+    :param b: int
+    :return: int
+
+    >>> add(2, 3)
+    {'result': 5}
+    """
+    return {'result': int(a) + int(b)}
---
+++ Bottle.py
@@ -0,0 +1,10 @@
+from bottle import route, run
+from Add import add
+
+
+@route('/hello/<a>/<b>')
+def hello(a, b):
+    return add(a, b)
+
+
+run(host='localhost', port=8080, debug=True)
